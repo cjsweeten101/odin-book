1eb95991d8f0309ba8569a8daf30d98686822150
Clayton <cjsweeten101@gmail.com>
2017-11-22 16:52:32 -0700

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
- 				 :rememberable, :omniauthable
+ 				 :rememberable, :omniauthable, :validatable
+ 	validates :firstname, presence: true
+ 	validates :lastname, presence: true
 end
