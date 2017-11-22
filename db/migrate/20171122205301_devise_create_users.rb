06424b4f8630fc505a40eae9acbd80dddd5ddc74
Clayton <cjsweeten101@gmail.com>
2017-11-22 14:00:02 -0700

    Devise user

---
+++ db/migrate/20171122205301_devise_create_users.rb
@@ -0,0 +1,20 @@
+class DeviseCreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      ## Database authenticatable
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+
+      ## Rememberable
+      t.datetime :remember_created_at
+
+      ## Basic User info
+      t.string :firstname,          null: false
+      t.string :lastname,           null: false
+
+      t.timestamps null: false
+    end
+
+    add_index :users, :email,                unique: true
+  end
+end
