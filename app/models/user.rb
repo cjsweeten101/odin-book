65876f8cf07331d6480742732dd2d24b30121b28
Clayton <cjsweeten101@gmail.com>
2017-11-22 17:01:38 -0700

    devise controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
  				 :rememberable, :omniauthable, :validatable
- 	validates :firstname, presence: true
- 	validates :lastname, presence: true
 end
