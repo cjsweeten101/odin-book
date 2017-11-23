d95fa856515529f0081e34df00bd41acb2732231
Clayton <cjsweeten101@gmail.com>
2017-11-22 20:03:14 -0700

    custom registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -8,9 +8,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    hi = configure_sign_up_params[:firstname]
+    super
+  end
 
   # GET /resource/edit
   # def edit
@@ -39,9 +40,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
