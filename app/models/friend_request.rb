fcc82038c81e98bd47ddaecd28c0e66221fe2e8c
Clayton <cjsweeten101@gmail.com>
2017-11-30 15:11:28 -0700

    validations for sending requests to friends

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -3,11 +3,15 @@ class FriendRequest < ApplicationRecord
 	belongs_to :friend, class_name: "User"
 
 	validate :can_not_friend_self
-
+	validate :can_not_friend_friends
 
 	private
 
 	def can_not_friend_self
 		@errors.add(:base, "Can't send friend request to yourself. . .") if self.user == self.friend
+	end
+
+	def can_not_friend_friends
+		@errors.add(:base, "Already friends. . .") if self.user.friends.include? self.friend
 	end
 end
